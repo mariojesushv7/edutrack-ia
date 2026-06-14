200f950e6bae49bf3bcce8d9b51e60897ca5c83e
Mario Hilari <marioestrun@gmail.com>
2026-06-14 11:02:41 -0400

    Arreglar envio de email en Render con puerto 587

--- backend/src/config/email.js
+++ backend/src/config/email.js
@@ -2,10 +2,15 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_PASS,
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
